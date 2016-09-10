8cc9caa1f560d715dc6ee9f83f169d0105d3afe5
chitownhope <apr_dec@hotmail.com>
2016-09-09 22:40:49 -0500

    another attempt

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,77 @@
 console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+/*not sure if i need EVERY possibility accounted for, but they are just in case*/
+if (cardOne === cardTwo){
+  message = "Great job! It's a match!";
+} else { 
+  message = "Sorry.  Not a match.  Try again!";  
+ } 
+
+if (cardOne === cardThree){
+  message = "Great job! It's a match!";
+} else {
+  message = "Sorry.  Not a match.  Try again!";  
+ } 
+
+if (cardOne === cardFour){
+  message = "Great job! It's a match!";
+} else {
+  message = "Sorry.  Not a match.  Try again!";   
+} 
+
+if (cardTwo === cardOne){
+  message = "Great job! It's a match!";
+} else {
+  message = "Sorry.  Not a match.  Try again!";  
+}  
+
+if (cardTwo === cardThree){
+  message = "Great job! It's a match!";
+} else {
+  message = "Sorry.  Not a match.  Try again!";  
+}  
+
+if (cardTwo === cardFour){
+  message = "Great job! It's a match!";
+} else {
+  message = "Sorry.  Not a match.  Try again!";  
+}  
+
+if (cardThree === cardOne){
+  message = "Great job! It's a match!";
+} else {
+  message = "Sorry.  Not a match.  Try again!";  
+} 
+
+if (cardThree === cardTwo){
+  message = "Great job! It's a match!";
+} else {
+  message = "Sorry.  Not a match.  Try again!";  
+} 
+
+if (cardThree === cardFour){
+  message = "Great job! It's a match!";
+} else {
+  message = "Sorry.  Not a match.  Try again!";  
+}
+
+if (cardFour === cardOne){
+  message = "Great job! It's a match!";
+} else {
+  message = "Sorry.  Not a match.  Try again!";  
+} 
+
+if (cardFour === cardTwo){
+  message = "Great job! It's a match!";
+} else {
+  message = "Sorry.  Not a match.  Try again!";  
+} 
+
+if (cardFour === cardThree){
+  message = "Great job! It's a match!";
+} else {
+  message = "Sorry.  Not a match.  Try again!";  
+} 
